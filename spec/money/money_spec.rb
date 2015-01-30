26e906d8025aa40b260e04f76491606a4fd96060
Abhimanyu <arya.abhimanyu.92@gmail.com>
2015-01-30 15:50:06 +0530

    modified specs to accomodate elaborated

--- spec/money/money_spec.rb
+++ spec/money/money_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe Money do
 
-  let(:money1) {Money.new(10,900)}
+  let(:money1) {Money.new(10,910)}
   let(:money2) {Money.new_rupee(10)}
   let(:money3) {Money.new_paise(900)}
 
   it "takes the value as a combination of rupees and paise" do
-    expect(money1.viewMoney).to eq(19.00)
+    expect(money1.view_money).to eq("19 rupees and 10 paise")
   end
 
   it "takes the value in rupees" do
-    expect(money2.viewMoney).to eq(10.00)
+    expect(money2.view_money).to eq("10 rupees and 0 paise")
   end
 
   it "takes the value in paise" do
-    expect(money3.viewMoney).to eq(9.00)
+    expect(money3.view_money).to eq("9 rupees and 0 paise")
   end
 
   it "adds value to the existing amount" do
-    expect(money1.addMoney(money2)).to eq(29.00)
+    expect(money1.addMoney(money2)).to eq(29.10)
   end
 
 end
