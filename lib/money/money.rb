667a5b0befcb61067bad860a3212e1a28c9c3e90
Abhimanyu <arya.abhimanyu.92@gmail.com>
2015-02-02 11:14:57 +0530

    added subtraction functionality to money class including the equality check as well

--- lib/money/money.rb
+++ lib/money/money.rb
@@ -17,17 +17,31 @@ class Money
 		self.new(0, paise)
 	end
 
-	def view_money
+	def to_s
 		val = @rupee + (0.01 * @paise)
 		val_rupee = val.to_i
 		val_paise = (val.modulo(1).round(2) * 100).to_i
 		"#{val_rupee} rupees and #{val_paise} paise"
 	end
 	
-  def add_money(moneyNew)
+    def +(money_new)
+		Money.new(@rupee + money_new.rupee, @paise + money_new.paise)
+	end
 
-  	Money.new(@rupee + moneyNew.rupee,@paise + moneyNew.paise)
-		
+	def -(money_new)
+
+		Money.new(@rupee - money_new.rupee, @paise - money_new.paise)
+	end
+
+	def ==(money_new)
+		return false unless money_new.is_a? Money
+		@rupee == money_new.rupee
+		@paise == money_new.paise
+	end
+
+	def eql?(money_new)
+		@rupee == money_new.rupee
+		@paise == money_new.paise
 	end
 
 end
